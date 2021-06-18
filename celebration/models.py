67c54634988272833e3db18fa415ecf8da4a2c2b
Jayant Jaiswal <63834490+jayantjaiswal100@users.noreply.github.com>
2021-06-18 20:51:47 +0530

    add list page for celebration

--- celebration/models.py
+++ celebration/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Celebration(models.Model):
     name = models.CharField(max_length=50)
     desc = models.TextField()
+    pic = models.ImageField(upload_to='celebration_list', height_field=None, width_field=None, max_length=None,null=True)
 
     def __str__(self):
         return self.name
