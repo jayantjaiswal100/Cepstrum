08b8ed3cf0715baae715054838a621ba3bb33f0f
Jayant Jaiswal <63834490+jayantjaiswal100@users.noreply.github.com>
2021-09-12 10:24:52 +0530

    Added Inplace API

--- inplace/urls.py
+++ inplace/urls.py
@@ -5,5 +5,7 @@ app_name='inplace'
 urlpatterns = [
       path('', views.InplaceListView.as_view(), name='all'),
       path('<int:pk>', views.PostDetailView.as_view(), name='detail'),
+      path('api', views.PaperManList.as_view()),
+      path('api/<int:pk>', views.PaperManRetrive.as_view()),
     
 ]
--- inplace/views.py
+++ inplace/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .models import Post
 # Create your views here.
 from django.views.generic import ListView, DetailView
+from rest_framework.generics import GenericAPIView
+from rest_framework import mixins
+from .serializers import PostSerializers
+# Create your views here.
 
 
 
@@ -14,5 +18,20 @@ class InplaceListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = "inplace/inplace_detail.html"
+
+
+class PostManList(mixins.ListModelMixin, GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializers
+
+    def get(self,request,*args,**kwargs):
+        return self.list(request,*args,**kwargs)
+
+class PostRetrive(mixins.RetrieveModelMixin, GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializers
+
+    def get(self,request,*args,**kwargs):
+        return self.retrieve(request,*args,**kwargs)
 
 
