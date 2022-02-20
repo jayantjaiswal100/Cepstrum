8b3170b36c0af0306c3e78f44344edbf08b41a98
Jayant Jaiswal <63834490+jayantjaiswal100@users.noreply.github.com>
2022-02-20 12:46:08 +0530

    final commit

--- inplace/urls.py
+++ inplace/urls.py
@@ -5,7 +5,6 @@ app_name='inplace'
 urlpatterns = [
       path('', views.InplaceListView.as_view(), name='all'),
       path('<int:pk>', views.PostDetailView.as_view(), name='detail'),
-      path('api', views.PostManList.as_view()),
-      path('api/<int:pk>', views.PostRetrive.as_view()),
+      
     
 ]
